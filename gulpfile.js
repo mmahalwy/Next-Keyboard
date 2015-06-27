4b00b0abd45752a45074aceb14d8826fc022b3b4
mmahalwy <mmahalwy@gmail.com>
2015-06-27 15:29:05 -0700

    Autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     gulp.src('public/styles/**/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+        }))
         .pipe(gulp.dest('public/styles'));
 });
 
