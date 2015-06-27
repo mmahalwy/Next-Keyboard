661a957fb7f959ee8f54780841e8dc57588116c4
mmahalwy <mmahalwy@gmail.com>
2015-06-26 18:55:40 -0700

    Almost there!

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,4 +7,12 @@
       $('nav').removeClass('active');
     }
   });
+
+  $('.toggle-modal').click(function() {
+    $('#video').attr('src', 'https://www.youtube.com/embed/Dcr-0AOQHts?autoplay=1&rel=0&amp;enablejsapi=1&amp;origin=http%3A%2F%2Fnextkeyboard.co');
+  });
+
+  $('#myModal').on('hide.bs.modal', function() {
+    $('#video').attr('src', 'https://www.youtube.com/embed/Dcr-0AOQHts?rel=0&amp;enablejsapi=1&amp;origin=http%3A%2F%2Fnextkeyboard.co');
+  });
 })(jQuery);
